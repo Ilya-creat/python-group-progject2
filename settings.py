fca062326cc8a54418bf812e760b67921411c78c
Retineizzer <a.batisov2017@yandex.ru>
2025-11-04 01:04:42 +0300

    Начал

--- settings.py
+++ settings.py
@@ -5,6 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 ODDS_API_KEY = os.getenv("ODDS_API_KEY")
+SSTATS_API_KEY = os.getenv("SSTATS_API_KEY")
 
 odds_session = create_session(base_url="https://api.the-odds-api.com",
                               params={"apiKey": ODDS_API_KEY})
+stats_session = create_session(base_url='https://api.sstats.net',
+                               extra_headers={'Authorization': f'ApiKey {SSTATS_API_KEY}'})
