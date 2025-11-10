199e7cdc29bdac6591da69a3fc910d3af27181ed
Ilya-creat <isaev.ilya2019@gmail.com>
2025-11-10 17:18:29 +0300

    fix config settings

--- settings.py
+++ settings.py
@@ -10,8 +10,3 @@ ODDS_API_KEY = os.getenv("ODDS_API_KEY")
 SSTATS_API_KEY = os.getenv("SSTATS_API_KEY")
 
 LIMIT_QUERY_ODDS_V4_SPORTS = 2
-
-odds_session = create_session(base_url="https://api.the-odds-api.com",
-                              params={"apiKey": ODDS_API_KEY})
-stats_session = create_session(base_url='https://api.sstats.net',
-                               extra_headers={'Authorization': f'ApiKey {SSTATS_API_KEY}'})
